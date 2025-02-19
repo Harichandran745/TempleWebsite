5fc98fee1a8c5167d41e1c2627a1dd5d023de6ea
Hari <137824193+user-nameas@users.noreply.github.com>
2025-02-19 13:23:15 +0530

    Volunteer page

--- js/Custom.js
+++ js/Custom.js
@@ -1,2 +1,32 @@
 // Set the current year dynamically
 document.getElementById("year").innerText = new Date().getFullYear();
+
+$(function () {
+    $(".form-input").each(function () {
+      changeClass($(this));
+    });
+  
+    $(".form-input").on("focusout", function () {
+      changeClass($(this));
+    });
+  
+    function changeClass($formInput) {
+      if ($formInput.val().length > 0) {
+        $formInput.addClass("has-value");
+      } else {
+        $formInput.removeClass("has-value");
+      }
+    }
+  
+    $(".date").datepicker();
+  
+    // Ensure the select box has the correct label behavior
+    $(".select-wrapper select").on("change", function () {
+      if ($(this).val() !== "none") {
+        $(this).addClass("has-value");
+      } else {
+        $(this).removeClass("has-value");
+      }
+    });
+  });
+  
